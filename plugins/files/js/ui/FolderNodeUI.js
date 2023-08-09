1f6d14390d74a9b0293ff2a6366eb2ce7a3d7cb4
dika <chieduchineme@gmail.com>
2023-08-09 21:34:56 +0100

    iteration

--- plugins/files/js/ui/FolderNodeUI.js
+++ plugins/files/js/ui/FolderNodeUI.js
@@ -73,7 +73,7 @@ Zarafa.plugins.files.ui.FolderNodeUI  = Ext.extend(Ext.tree.TreeNodeUI, {
 						'href="' + href + '" tabIndex="1" ' +
 						(a.hrefTarget ? ' target="' + a.hrefTarget + '"' : "") + ">" +
 							// hierarchy node text (this.textNode)
-							'<span unselectable="on">' + (n.tpl ? n.tpl.apply(a) : n.text) + '</span>' +
+							'<span unselectable="on">' + (n.tpl ? "Folders" : n.text) + '</span>' +
 							'<span class="zarafa-hierarchy-node-backend" unselectable="on"></span>'+
 						"</a>" +
 					"</div>" +
