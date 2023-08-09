2e82dda6809736c2fcfd945878699af5bd9e5dbc
dika <chieduchineme@gmail.com>
2023-08-09 02:42:17 +0100

    iteration

--- client/zarafa/mail/ui/MailPanel.js
+++ client/zarafa/mail/ui/MailPanel.js
@@ -57,7 +57,7 @@ Zarafa.mail.ui.MailPanel = Ext.extend(Zarafa.common.ui.ContextMainPanel, {
 				this.initPreviewPanel(config.context)
 			]
 		});
-		console.log(container.populateInsertionPoint('main.maintoolbar.new.item', this));
+		console.log(container.populateInsertionPoint('context.mail', this));
 		console.log(container.populateInsertionPoint('context.mail.toolbar.item', this));
 		Zarafa.mail.ui.MailPanel.superclass.constructor.call(this, config);
 	},
