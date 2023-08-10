8e08c9dbd66644a18794e25648c35aeb24d372a0
dika <chieduchineme@gmail.com>
2023-08-10 16:31:36 +0100

    add Home button to HierarchyTreePanel

--- client/zarafa/hierarchy/ui/FolderNodeUI.js
+++ client/zarafa/hierarchy/ui/FolderNodeUI.js
@@ -134,7 +134,7 @@ Zarafa.hierarchy.ui.FolderNodeUI = Ext.extend(Ext.tree.TreeNodeUI, {
 		}
 
 		this.elNode = this.wrap.childNodes[0];
-		console.log(this.elNode);
+		console.log(this.wrap);
 		this.ctNode = this.wrap.childNodes[1];
 		var cs = this.elNode.childNodes;
 		this.indentNode = cs[0];
