642673929e21ec60f5c0bbfbb17f7040482822cc
dika <chieduchineme@gmail.com>
2023-08-18 21:05:08 +0100

    figma for sidebar

--- client/zarafa/hierarchy/ui/FolderNodeUI.js
+++ client/zarafa/hierarchy/ui/FolderNodeUI.js
@@ -134,7 +134,7 @@ Zarafa.hierarchy.ui.FolderNodeUI = Ext.extend(Ext.tree.TreeNodeUI, {
 
 		this.elNode = this.wrap.childNodes[0];
 		this.ctNode = this.wrap.childNodes[1];
-		console.log(this.elNode);
+		console.log(this.elNode[1]);
 		var cs = this.elNode.childNodes;
 		this.indentNode = cs[0];
 		this.ecNode = cs[1];
