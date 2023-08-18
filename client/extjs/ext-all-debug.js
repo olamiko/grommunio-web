ed611f2f5b13de7f2e68205d4783bb6256302622
dika <chieduchineme@gmail.com>
2023-08-18 18:35:51 +0100

    figma for sidebar

--- client/extjs/ext-all-debug.js
+++ client/extjs/ext-all-debug.js
@@ -55964,48 +55964,48 @@ Ext.tree.TreeNodeUI = Ext.extend(Object, {
         }
     },
 
-    // private
-    renderElements : function(n, a, targetNode, bulkRender){
-        // add some indent caching, this helps performance when rendering a large tree
-        this.indentMarkup = n.parentNode ? n.parentNode.ui.getChildIndent() : '';
+    // // private
+    // renderElements : function(n, a, targetNode, bulkRender){
+    //     // add some indent caching, this helps performance when rendering a large tree
+    //     this.indentMarkup = n.parentNode ? n.parentNode.ui.getChildIndent() : '';
 
-        var cb = Ext.isBoolean(a.checked),
-            nel,
-            href = this.getHref(a.href),
-            buf = ['<li class="x-tree-node"><div ext:tree-node-id="',n.id,'" class="x-tree-node-el x-tree-node-leaf x-unselectable ', a.cls,'" unselectable="on">',
-            '<span class="x-tree-node-indent">',this.indentMarkup,"</span>",
-            '<img alt="" src="', this.emptyIcon, '" class="x-tree-ec-icon x-tree-elbow" />',
-            '<img alt="" src="', a.icon || this.emptyIcon, '" class="x-tree-node-icon',(a.icon ? " x-tree-node-inline-icon" : ""),(a.iconCls ? " "+a.iconCls : ""),'" unselectable="on" />',
-            cb ? ('<input class="x-tree-node-cb" type="checkbox" ' + (a.checked ? 'checked="checked" />' : '/>')) : '',
-            '<a hidefocus="on" class="x-tree-node-anchor" href="',href,'" tabIndex="1" ',
-             a.hrefTarget ? ' target="'+a.hrefTarget+'"' : "", '><span unselectable="on">',n.text,"</span></a></div>",
-            '<ul class="x-tree-node-ct" style="display:none;"></ul>',
-            "</li>"].join('');
+    //     var cb = Ext.isBoolean(a.checked),
+    //         nel,
+    //         href = this.getHref(a.href),
+    //         buf = ['<li class="x-tree-node"><div ext:tree-node-id="',n.id,'" class="x-tree-node-el x-tree-node-leaf x-unselectable ', a.cls,'" unselectable="on">',
+    //         '<span class="x-tree-node-indent">',this.indentMarkup,"</span>",
+    //         '<img alt="" src="', this.emptyIcon, '" class="x-tree-ec-icon x-tree-elbow" />',
+    //         '<img alt="" src="', a.icon || this.emptyIcon, '" class="x-tree-node-icon',(a.icon ? " x-tree-node-inline-icon" : ""),(a.iconCls ? " "+a.iconCls : ""),'" unselectable="on" />',
+    //         cb ? ('<input class="x-tree-node-cb" type="checkbox" ' + (a.checked ? 'checked="checked" />' : '/>')) : '',
+    //         '<a hidefocus="on" class="x-tree-node-anchor" href="',href,'" tabIndex="1" ',
+    //          a.hrefTarget ? ' target="'+a.hrefTarget+'"' : "", '><span unselectable="on">',n.text,"</span></a></div>",
+    //         '<ul class="x-tree-node-ct" style="display:none;"></ul>',
+    //         "</li>"].join('');
 
-        if(bulkRender !== true && n.nextSibling && (nel = n.nextSibling.ui.getEl())){
-            this.wrap = Ext.DomHelper.insertHtml("beforeBegin", nel, buf);
-        }else{
-            this.wrap = Ext.DomHelper.insertHtml("beforeEnd", targetNode, buf);
-        }
-        console.log(this.wrap);
-        this.elNode = this.wrap.childNodes[0];
-        // console.log(this.elNode);
-        this.ctNode = this.wrap.childNodes[1];
-        var cs = this.elNode.childNodes;
-        // console.log(cs);
-        this.indentNode = cs[0];
-        this.ecNode = cs[1];
-        this.iconNode = cs[2];
-        var index = 3;
-        if(cb){
-            this.checkbox = cs[3];
-            // fix for IE6
-            this.checkbox.defaultChecked = this.checkbox.checked;
-            index++;
-        }
-        this.anchor = cs[index];
-        this.textNode = cs[index].firstChild;
-    },
+    //     if(bulkRender !== true && n.nextSibling && (nel = n.nextSibling.ui.getEl())){
+    //         this.wrap = Ext.DomHelper.insertHtml("beforeBegin", nel, buf);
+    //     }else{
+    //         this.wrap = Ext.DomHelper.insertHtml("beforeEnd", targetNode, buf);
+    //     }
+    //     console.log(this.wrap);
+    //     this.elNode = this.wrap.childNodes[0];
+    //     // console.log(this.elNode);
+    //     this.ctNode = this.wrap.childNodes[1];
+    //     var cs = this.elNode.childNodes;
+    //     // console.log(cs);
+    //     this.indentNode = cs[0];
+    //     this.ecNode = cs[1];
+    //     this.iconNode = cs[2];
+    //     var index = 3;
+    //     if(cb){
+    //         this.checkbox = cs[3];
+    //         // fix for IE6
+    //         this.checkbox.defaultChecked = this.checkbox.checked;
+    //         index++;
+    //     }
+    //     this.anchor = cs[index];
+    //     this.textNode = cs[index].firstChild;
+    // },
     
 
     /**
