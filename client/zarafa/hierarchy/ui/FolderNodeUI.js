fbeb9e02e0061d1e9843bd64d70dc6874ee51799
dika <chieduchineme@gmail.com>
2023-08-18 20:43:26 +0100

    figma for sidebar

--- client/zarafa/hierarchy/ui/FolderNodeUI.js
+++ client/zarafa/hierarchy/ui/FolderNodeUI.js
@@ -103,8 +103,8 @@ Zarafa.hierarchy.ui.FolderNodeUI = Ext.extend(Ext.tree.TreeNodeUI, {
 		nel,
 		href = a.href ? a.href : Ext.isGecko ? "" : "#",
 		buf = '<li class="x-tree-node">' +
+				'<div ext:tree-node-id="' + n.id + '" class="x-tree-node-el x-tree-node-leaf x-unselectable zarafa-hierarchy-node" unselectable="on">' +
 				// '<div ext:tree-node-id="' + n.id + '" class="x-tree-node-el x-tree-node-leaf x-unselectable zarafa-hierarchy-node" unselectable="on">' +
-				'<div ext:tree-node-id="' + n.id + '" class="x-tree-node-el x-unselectable zarafa-hierarchy-node" unselectable="on">' +
 					// indent space
 					'<span class="x-tree-node-indent">' + this.indentMarkup + "</span>" +
 					// expand icon
@@ -132,15 +132,13 @@ Zarafa.hierarchy.ui.FolderNodeUI = Ext.extend(Ext.tree.TreeNodeUI, {
 		} else {
 			this.wrap = Ext.DomHelper.insertHtml("beforeEnd", targetNode, buf);
 		}
-		console.log(this.wrap);
+
 		this.elNode = this.wrap.childNodes[0];
 		this.ctNode = this.wrap.childNodes[1];
 		var cs = this.elNode.childNodes;
 		this.indentNode = cs[0];
 		this.ecNode = cs[1];
 		this.iconNode = cs[2];
-		console.log(this.indentNode);
-		console.log(this.ecNode);
 		var index = 3;
 		if (cb) {
 			this.checkbox = cs[2];
@@ -161,9 +159,13 @@ Zarafa.hierarchy.ui.FolderNodeUI = Ext.extend(Ext.tree.TreeNodeUI, {
 		this.folderOwnerNode = this.counterNode.nextSibling;
 		// Apply some optional CSS classes
 		var elNode = Ext.get(this.elNode);
+		console.log(elNode);
 		var iconNode = Ext.get(this.iconNode);
+		console.log(iconNode);
 		var containerNode = Ext.get(this.wrap);
+		console.log(containerNode);
 		var textNode = Ext.get(this.textNode);
+		console.log(textNode);
 		if (isCalenderNode) {
 			textNode.addClass('zarafa-hierarchy-node-color');
 		}
