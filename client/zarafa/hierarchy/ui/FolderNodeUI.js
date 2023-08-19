226614660d7696650698fdaf513563adcbd0286c
dika <chieduchineme@gmail.com>
2023-08-19 18:25:14 +0100

    figma for sidebar

--- client/zarafa/hierarchy/ui/FolderNodeUI.js
+++ client/zarafa/hierarchy/ui/FolderNodeUI.js
@@ -156,35 +156,36 @@ Zarafa.hierarchy.ui.FolderNodeUI = Ext.extend(Ext.tree.TreeNodeUI, {
 		// newElement.textContent = "Home";
 		// } 
 		// Get the element by ID
-	const hierarchyTreePanel = document.getElementsByClassName("zarafa-hierarchy-treepanel-body");
+// Get elements by class name
+const hierarchyTreePanels = document.getElementsByClassName("zarafa-hierarchy-treepanel-body");
 
-	if (hierarchyTreePanel) {
-	// Find the first div child with class "x-tree-node-el"
-	const firstDivChild = hierarchyTreePanel.querySelector("div.x-tree-node-el");
+// Loop through each element with the specified class
+for (const hierarchyTreePanel of hierarchyTreePanels) {
+  // Find the first div child with class "x-tree-node-el"
+  const firstDivChild = hierarchyTreePanel.querySelector("div.x-tree-node-el");
 
-	if (firstDivChild) {
-		// Remove the class "x-tree-node-el" from the div
-		firstDivChild.classList.remove("x-tree-node-el");
-	}
+  if (firstDivChild) {
+    // Remove the class "x-tree-node-el" from the div
+    firstDivChild.classList.remove("x-tree-node-el");
+  }
 
-	// Find the first span child
-	const firstSpanChild = hierarchyTreePanel.querySelector("span:first-child");
+  // Find the first span child
+  const firstSpanChild = hierarchyTreePanel.querySelector("span:first-child");
 
-	if (firstSpanChild) {
-		// Set the display to "none" for the first span child
-		firstSpanChild.style.display = "none";
-	}
+  if (firstSpanChild) {
+    // Set the display to "none" for the first span child
+    firstSpanChild.style.display = "none";
+  }
 
-	// Find the second span child
-	const secondSpanChild = hierarchyTreePanel.querySelector("span:nth-child(2)");
+  // Find the second span child
+  const secondSpanChild = hierarchyTreePanel.querySelector("span:nth-child(2)");
 
-	if (secondSpanChild) {
-		// Replace the text content with "Home"
-		secondSpanChild.textContent = "Home";
-	}
-	} else {
-	console.log("Element 'zarafa-hierarchy-treepanel-body' not found.");
-	}
+  if (secondSpanChild) {
+    // Replace the text content with "Home"
+    secondSpanChild.textContent = "Home";
+  }
+}
+
 
 		this.elNode = this.wrap.childNodes[0];
 		this.ctNode = this.wrap.childNodes[1];
